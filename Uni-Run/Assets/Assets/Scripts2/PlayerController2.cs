07c7ab206c066a56c1c37be3c9db569ffb0431b0
honeypolarbear <cjkim6375@naver.com>
2022-02-22 12:48:17 +0900

    20220222
    2222

--- Uni-Run/Assets/Assets/Scripts2/PlayerController2.cs
+++ Uni-Run/Assets/Assets/Scripts2/PlayerController2.cs
@@ -18,29 +18,77 @@ public class PlayerController2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerrigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDead)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0) && jumpCount<2)
+        {
+
+            jumpCount++;
+
+            playerrigidbody.velocity = Vector2.zero;
+
+            playerrigidbody.AddForce(new Vector2(0, jumpForce));
+
+            playerAudio.Play();
+        }
+
+        else if (Input.GetMouseButtonUp(0) && playerrigidbody.velocity.y > 0)
+        {
+            playerrigidbody.velocity = playerrigidbody.velocity * 0.5f;
+        }
+
+        animator.SetBool("Grounded", isGrounded);
     }
     private void Die()
     {
+        animator.SetTrigger("Die");
 
+
+        playerAudio.clip = deathClip;
+
+        playerAudio.Play();
+
+        playerrigidbody.velocity = Vector2.zero;
+
+        isDead = true;
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Dead" && isDead)
+        {
+            Die();
+        }
     }
-    private void OnCollisionEnter2D(Collision collision)
-    {
+    //private void OnCollisionEnter2D(Collision collision)
+    //{
+    //    if (collision.contacts[0].normal.y> 0.7f)
+    //    {
+    //        isGrounded = true;
+    //        jumpCount = 0;
+    //    }
+    //}
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        
+        isGrounded = false;
     }
 
 
