60a76920ab32fd4d80fa8d29ad218be55ad36151
honeypolarbear <cjkim6375@naver.com>
2022-02-24 13:35:13 +0900

    20220224
    1111

---
+++ Uni-Run/Assets/Assets/Scripts2/BackgroundLoop2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop2 : MonoBehaviour
+{
+    private float width;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform .position.x <= -width)
+        {
+            Reposition();
+        }
+    }
+    private void Reposition()
+    {
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+}
