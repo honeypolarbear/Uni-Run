80f3f9733b73c523133d7312b1c7fda9a7bddf07
honeypolarbear <cjkim6375@naver.com>
2022-03-10 12:48:45 +0900

    20220310

--- Uni-Run/Assets/Assets/Scripts2/Platform2.cs
+++ Uni-Run/Assets/Assets/Scripts2/Platform2.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Platform2 : MonoBehaviour
 {
+    public GameObject[] obstacles;// 장애물 오브젝트들
+    private bool stepped = false;//플레이어 캐릭터가 밟았는가
     // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        //플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 자리
     }
 }
